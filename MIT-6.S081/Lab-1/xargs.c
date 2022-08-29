f73de59438366206f06b9e31378284c0f86d4af6
Vishal Choudhary <contactvishaltech@gmail.com>
2022-08-29 10:12:27 +0530

    xargs.c

--- MIT-6.S081/Lab-1/xargs.c
+++ MIT-6.S081/Lab-1/xargs.c
@@ -6,14 +6,26 @@ int main(int argc, char *argv[]) {
   char buff[512] = {0};
   read(0, buff, 512);
 
+  int pid, status;
   char *argvs[MAXARG] = {0};
 
   int idx = 0;
   for (int i = 1; i < argc; i++) {
     argvs[idx++] = argv[i];
   }
-  argvs[idx] = buff;
-  exec(argvs[0], argvs);
-  printf("exec failed!\n");
-  exit(1);
+  argvs[idx++] = buff;
+
+  for (int i = 1; i < idx; i++) {
+    pid = fork();
+    if (pid == 0) {
+      char *newArgs[] = {argvs[0], argvs[i], 0};
+      exec(argvs[0], newArgs);
+      printf("exec failed!\n");
+      exit(1);
+    } else {
+      wait(&status);
+    }
+  }
+
+  exit(0);
 }

cfdcf3ec5e7a7121bd09cc54fa0cd9a71e2d7955
Vishal Choudhary <contactvishaltech@gmail.com>
2022-09-01 22:28:16 +0530

    completed lab01

--- UCB-CS-61C/lab01/ll_cycle.c
+++ UCB-CS-61C/lab01/ll_cycle.c
@@ -2,6 +2,20 @@
 #include "ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-    /* your code here */
+    if(head == NULL){
+        return 0;
+    }
+    node *ptr1 = head, *ptr2 = head->next;
+    while(ptr1 != NULL && ptr2 != NULL){
+        if(ptr1 == ptr2){
+            return 1;
+        }
+        ptr1 = ptr1->next;
+        ptr2 = ptr2->next;
+        if(ptr2 == NULL){
+            break;
+        }
+        ptr2 = ptr2->next;
+    }
     return 0;
 }

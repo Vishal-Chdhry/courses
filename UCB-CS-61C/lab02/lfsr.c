28edb4f0fd49be9098e3b2f29a81b7843fcaaeae
Vishal Choudhary <contactvishaltech@gmail.com>
2022-09-09 16:35:24 +0530

    lsfr

--- UCB-CS-61C/lab02/lfsr.c
+++ UCB-CS-61C/lab02/lfsr.c
@@ -4,7 +4,21 @@
 #include <string.h>
 #include "lfsr.h"
 
-void lfsr_calculate(uint16_t *reg) {
-    /* YOUR CODE HERE */
-}
+void
+lfsr_calculate (uint16_t *reg)
+{
+ /* YOUR CODE HERE */
+ int bit = 0;
+ if (*reg & 1)
+  bit ^= 1;
+ if (*reg & (1 << 2))
+  bit ^= 1;
+ if (*reg & (1 << 3))
+  bit ^= 1;
+ if (*reg & (1 << 5))
+  bit ^= 1;
+ *reg = *reg >> 1;
 
+ if (bit)
+  *reg |= (1 << 15);
+}
